d49c34950a3cfd50f58d1e5cc963c402bacc5671
shivani Pitta <shivanipitta@shivanis-mbp.attlocal.net>
2018-12-11 08:23:09 -0500

    initial angular commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { SearchBar } from '@angular/ionic';
+import {
+  MatToolbarModule,
+  MatTabsModule,
+  MatButtonModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatRadioModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatListModule,
+  MatGridListModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {ScrollDispatchModule} from '@angular/cdk/scrolling';
 
 import { AppComponent } from './app.component';
+import { GroupByPipe } from './shared/groupby.pipe';
+import { KeyValuePipe } from './shared/key-value.pipe';
+import { UnCamelCasePipe } from './shared/un-camel-case.pipe';
+// import {MatInputModule} from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { FilterPipe} from './shared/filter.pipe';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule,
+  MatExpansionModule,
+    MatButtonModule, MatIconModule,
+    MatTabsModule, FlexLayoutModule,ScrollDispatchModule,
+  MatFormFieldModule, MatCardModule,MatListModule, MatGridListModule, MatInputModule, MatAutocompleteModule],
+  declarations: [AppComponent, GroupByPipe, KeyValuePipe, UnCamelCasePipe, FilterPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
