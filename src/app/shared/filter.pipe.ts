a9baa05e98a0923f1e006e427a1382d43335f4fd
shivani Pitta <shivanipitta@shivanis-mbp.attlocal.net>
2018-12-12 09:09:20 -0500

    removed test.ts

--- src/app/shared/filter.pipe.ts
+++ src/app/shared/filter.pipe.ts
@@ -4,8 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   transform(items: any[], searchText: string): any[] {
-    if(!items) return [];
-    if(!searchText) return items;
+    if(!items)
+    { return [];
+    }
+    if(!searchText) {
+      return items;
+    }
 searchText = searchText.toLowerCase();
 return items.filter( it => {
       return it.president.toLowerCase().includes(searchText) || it.party.toLowerCase().includes(searchText) || it.took_office.toLowerCase().includes(searchText);
