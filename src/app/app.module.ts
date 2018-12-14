2e675840c1d1a1b64d62c60365de1c000c52b02a
shivani Pitta <shivanipitta@shivanis-mbp.attlocal.net>
2018-12-14 14:57:34 -0500

    removed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,16 @@ import {
   MatTabsModule,
   MatButtonModule,
   MatInputModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatCheckboxModule,
   MatIconModule,
-  MatRadioModule,
-  MatExpansionModule,
   MatFormFieldModule,
   MatCardModule,
   MatListModule,
-  MatGridListModule
+  
 } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {ScrollDispatchModule} from '@angular/cdk/scrolling';
 
 import { AppComponent } from './app.component';
-import { GroupByPipe } from './shared/groupby.pipe';
-import { KeyValuePipe } from './shared/key-value.pipe';
-import { UnCamelCasePipe } from './shared/un-camel-case.pipe';
 // import {MatInputModule} from '@angular/material/input';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { FilterPipe} from './shared/filter.pipe';
@@ -33,11 +25,11 @@ import { FilterPipe} from './shared/filter.pipe';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule,
-  MatExpansionModule,
+
     MatButtonModule, MatIconModule,
     MatTabsModule, FlexLayoutModule,ScrollDispatchModule,
-  MatFormFieldModule, MatCardModule,MatListModule, MatGridListModule, MatInputModule, MatAutocompleteModule],
-  declarations: [AppComponent, GroupByPipe, KeyValuePipe, UnCamelCasePipe, FilterPipe],
+  MatFormFieldModule, MatCardModule,MatListModule,MatInputModule, MatAutocompleteModule],
+  declarations: [AppComponent, FilterPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
