7573cf7d17bb699cd4e9f5afc3d6f3edde1765ef
shivani Pitta <shivanipitta@shivanis-MacBook-Pro.local>
2019-01-02 21:52:08 -0500

    made changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { SearchBar } from '@angular/ionic';
 import {
   MatToolbarModule,
   MatTabsModule,
@@ -10,25 +8,18 @@ import {
   MatInputModule,
   MatIconModule,
   MatFormFieldModule,
-  MatCardModule,
-  MatListModule,
-  
+  MatListModule
 } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {ScrollDispatchModule} from '@angular/cdk/scrolling';
-
 import { AppComponent } from './app.component';
-// import {MatInputModule} from '@angular/material/input';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { FilterPipe} from './shared/filter.pipe';
 
-
 @NgModule({
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule,
-
+  imports: [BrowserModule, FormsModule, MatToolbarModule,
+  
     MatButtonModule, MatIconModule,
-    MatTabsModule, FlexLayoutModule,ScrollDispatchModule,
-  MatFormFieldModule, MatCardModule,MatListModule,MatInputModule, MatAutocompleteModule],
+    MatTabsModule, FlexLayoutModule,
+  MatFormFieldModule,MatListModule, MatInputModule],
   declarations: [AppComponent, FilterPipe],
   bootstrap: [AppComponent]
 })
