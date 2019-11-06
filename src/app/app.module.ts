906f93aa3df35f5a3609ac50cc4003f174b7e1bd
Chinna Gurrala <cm.reddy071@gmail.com>
2019-11-06 15:24:33 -0500

    created ionic search-bar example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
+
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { FilterPipe } from './shared/filter';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FilterPipe],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatListModule,
+    ScrollDispatchModule,
+    BrowserAnimationsModule,
+    FormsModule
+  ],
   providers: [
     StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    SplashScreen
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
