7f960677334d249412f23bf5b291f26ec8b7625b
Chinna Gurrala <cm.reddy071@gmail.com>
2019-11-13 10:48:55 -0500

    Use themes

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Platform } from 'react-native';
 import * as Colors from '@pxblue/colors'
 import { Header, wrapIcon } from '@pxblue/react-native-components';
 import MatIcon from 'react-native-vector-icons/MaterialIcons';
 import { ListItem } from 'react-native-elements';
+import { ThemeProvider } from '@pxblue/react-native-components';
+import * as PXBThemes from '@pxblue/themes/react-native';
+import * as Font from 'expo-font';
 
 import sampleData from './data';
-import AnimatedSearchBar from './AnimatedSearchBar';
 
 const MenuIcon = wrapIcon({ IconClass: MatIcon, name: 'menu' });
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { showSearch: false, query: '', results: sampleData, data: sampleData };
+    this.state = { query: '', results: sampleData, data: sampleData, fontLoaded: false };
   }
+  async componentDidMount() {
+    await Font.loadAsync({
+      'OpenSans-Extrabold': require('./assets/fonts/OpenSans-ExtraBold.ttf'),
+      'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+      'OpenSans-SemiBold': require('./assets/fonts/OpenSans-SemiBold.ttf'),
+      'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'OpenSans-Light': require('./assets/fonts/OpenSans-Light.ttf'),
+    });
 
-  onSearchToggle = () => this.setState((prevState) => ({ showSearch: !prevState.showSearch }));
+    this.setState({ fontLoaded: true });
+  }
   onSearchChange = (query) => this.setState({ query }, this.onSearch)
   onSearch = () => {
     const query = this.state.query.toLowerCase().trim();
@@ -31,59 +42,56 @@ class App extends React.Component {
     });
     this.setState({ results });
   }
-  onClear = () => {
-    this.setState({ results: this.state.data, query: '', showSearch: false });
-  }
+
   render() {
+    if (!this.state.fontLoaded) {
+      return null;
+    }
     return (
-      <View style={styles.container}>
-        <Header
-          title={'President'}
-          subtitle={'Leader of the Free world'}
-          backgroundColor={Colors.blue[500]}
-          navigation={{ icon: MenuIcon, onPress: () => { } }}
-          searchableConfig={{
-            placeholder: 'Search',
-            onChangeText: (q) => this.onSearchChange(q)
-          }}
-        />
-        <AnimatedSearchBar
-          show={this.state.showSearch}
-          value={this.state.query}
-          onClear={this.onClear}
-          onChangeText={this.onSearchChange}
-        />
-        {
-          this.state.results.length === 0
-            ? (
-              <ListItem
-                title="0 results"
-                subtitle="No matching presidents"
-                subtitleStyle={{ color: Colors.gray[500] }}
-                leftIcon={{ name: 'info', color: Colors.gray[500] }}
-              />
-            )
-            : (
-              <FlatList
-                data={this.state.results}
-                keyExtractor={(item, index) => `${index}`}
-                renderItem={({ item }) => (
-                  <ListItem
-                    title={item.president}
-                    subtitle={(<View style={{ marginLeft: 15 }}>
-                      <Text style={{ color: Colors.gray[500] }}>{item.party}</Text>
-                      <Text style={{ color: Colors.gray[500] }}>{item.took_office}</Text>
-                    </View>)}
-                    titleStyle={{ marginLeft: 15 }}
-                    leftIcon={{ name: 'person', color: Colors.gray[500], iconStyle: { marginLeft: 3 } }}
-                  />
-                )}
-              />
-            )
-        }
+      <ThemeProvider theme={PXBThemes.blue}>
+        <View style={styles.container}>
+          <Header
+            title={'President'}
+            subtitle={'Leader of the Free world'}
+            navigation={{ icon: MenuIcon, onPress: () => { } }}
+            searchableConfig={{
+              placeholder: 'Search',
+              onChangeText: (q) => this.onSearchChange(q)
+            }}
+          />
+          {
+            this.state.results.length === 0
+              ? (
+                <ListItem
+                  title="0 results"
+                  subtitle="No matching presidents"
+                  titleStyle={{ marginLeft: 16 }}
+                  subtitleStyle={{ color: Colors.gray[500], marginLeft: 16 }}
+                  leftIcon={{ name: 'info', color: Colors.gray[500], iconStyle: { marginLeft: 3 } }}
+                />
+              )
+              : (
+                <FlatList
+                  data={this.state.results}
+                  keyExtractor={(item, index) => `${index}`}
+                  renderItem={({ item }) => (
+                    <ListItem
+                      title={item.president}
+                      subtitle={(<View style={{ marginLeft: 16 }}>
+                        <Text style={{ color: Colors.gray[500] }}>{item.party}</Text>
+                        <Text style={{ color: Colors.gray[500] }}>{item.took_office}</Text>
+                      </View>)}
+                      titleStyle={{ marginLeft: 16 }}
+                      leftIcon={{ name: 'person', color: Colors.gray[500], iconStyle: { marginLeft: 3 } }}
+                    />
+                  )}
+                />
+              )
+          }
 
 
-      </View>
+        </View>
+      </ThemeProvider>
     )
   }
 }
@@ -91,7 +99,8 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.white[50]
+    backgroundColor: Colors.white[50],
+    marginTop: Platform.OS === 'android' ? 24 : 0
   },
 });
 
