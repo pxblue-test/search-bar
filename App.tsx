700a8be0f73ee8cdbd9a9dd170df0473a0b80041
huayunh <hhuayun0828@gmail.com>
2020-01-20 13:42:02 -0500

    convert to typescript; add lint and prettier

---
+++ App.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { ThemeProvider } from '@pxblue/react-native-components';
+import List from './List';
+
+//@ts-ignore
+import { ReactNativeThemes } from '@pxblue/themes';
+
+import * as Font from 'expo-font';
+
+//@ts-ignore
+import extraBoldFont from './assets/fonts/OpenSans-ExtraBold.ttf';
+//@ts-ignore
+import boldFont from './assets/fonts/OpenSans-Bold.ttf';
+//@ts-ignore
+import semiBoldFont from './assets/fonts/OpenSans-SemiBold.ttf';
+//@ts-ignore
+import regularFont from './assets/fonts/OpenSans-Regular.ttf';
+//@ts-ignore
+import lightFont from './assets/fonts/OpenSans-Light.ttf';
+
+type TProps = {};
+
+type TState = {
+    fontLoaded: boolean;
+};
+
+class App extends React.Component<TProps, TState> {
+    constructor(props: TProps) {
+        super(props);
+        this.state = { fontLoaded: false };
+    }
+    async componentDidMount(): Promise<void> {
+        await Font.loadAsync({
+            'OpenSans-ExtraBold': extraBoldFont,
+            'OpenSans-Bold': boldFont,
+            'OpenSans-SemiBold': semiBoldFont,
+            'OpenSans-Regular': regularFont,
+            'OpenSans-Light': lightFont
+        });
+
+        this.setState({ fontLoaded: true });
+    }
+
+    render(): JSX.Element | null {
+        if (!this.state.fontLoaded) {
+            return null;
+        }
+        return (
+            <ThemeProvider theme={ReactNativeThemes.blue}>
+                <List />
+            </ThemeProvider>
+        );
+    }
+}
+
+export default App;
